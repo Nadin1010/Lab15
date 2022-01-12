17f6f2416edee637a46d9f8515cd2d93c211ef50
Надежда Кузнецова <kuznecovanadezhd@gmail.com>
2022-01-13 01:27:27 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ Lab15/GeomProgression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    
+        /*
+     метод void setStart(int x) - устанавливает начальное значение
+   метод int getNext() - возвращает следующее число ряда
+   метод void reset() - выполняет сброс к начальному значению
+    */
+
+        class GeomProgression : ISeries
+        {
+            int step;
+            int startValue;
+            int currentValue;
+
+            public int getNext()
+            {
+                currentValue *= step;
+                return currentValue;
+            }
+
+            public void reset()
+            {
+                currentValue = startValue;
+            }
+
+            public void setStart(int x)
+            {
+                startValue = x;
+                currentValue = startValue;
+            }
+            public void setStep(int s)
+            {
+                step = s;
+            }
+        }
+    }
+
+
+  
---
+++ Lab15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(20);
+            arith.setStep(4);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            var geom = new GeomProgression();
+            geom.setStart(20);
+            geom.setStep(4);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
+        
+   
+    
+
+
+
+
+
+
